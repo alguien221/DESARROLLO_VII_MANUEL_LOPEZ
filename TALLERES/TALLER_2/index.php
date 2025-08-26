0f482bbe209beb3a5f8146dfbacda9e21c6b1c65
Manuel Lopez <manuel.isaac.lopez@gmail.com>
2025-08-25 21:20:32 -0500

    Agregados ejemplos de impresión en PHP

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,26 +1,20 @@
-<?php 
-$nombre = "Carlos";
-$edad = 24;
-$altura = 1.69;
-$esEstudiante = true;
 
-// Concatenación usando el operador
-$presentacion1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+<?php
+$nombre = "Juan";
+$edad = 25;
 
-// Concatenación  dentro de comillas dobles
-$presentacion2 = "Hola, mi nombre es $nombre y tengo $edad años.";
+// Usando echo
+echo "Hola, mundo!<br>";
+echo "Mi nombre es $nombre<br>";
 
-// Definir una constante
-define("SALUDO", "Bienvenido!");
+// Usando print
+print "Tengo $edad años<br>";
 
-// Concatenar una constante
-$mensaje = SALUDO . " " . $nombre;
+// Usando printf (permite formateo)
+printf("Me llamo %s y tengo %d años<br>", $nombre, $edad);
 
-echo $presentacion1 . "<br>";
-echo $presentacion2 . "<br>";
-echo $mensaje . "<br>";
-echo "Nombre: $nombre<br>";
-echo "Edad: $edad<br>";
-echo "Altura: $altura<br>";
-echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
+// Usando var_dump (útil para debugging)
+var_dump($nombre);
+echo "<br>";
 ?>
+                    
