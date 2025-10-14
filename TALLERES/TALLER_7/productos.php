360443b4654f21aa1bbbc178e48345048d31a3e9
Manuel Lopez <39249889+alguien221@users.noreply.github.com>
2025-10-13 20:09:12 -0500

    TALLER_7 - Implementación del sistema de carrito de compras

---
+++ TALLERES/TALLER_7/productos.php
@@ -0,0 +1,21 @@
+<?php
+
+$productos = [
+    "platos" => 1.5,
+    "manteles" => 7,
+    "jabon" => 1.75,
+    "cubiertos" => 5,
+    "jarras"=> 3,
+]
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+
+    </body>
+</html>
